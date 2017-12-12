d25df11e27884069f6c27a7b80e54cc89795cbce
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2017-12-12 01:39:12 -0800

    Added comments and followed PEP and added another exercise in Chapter 5

---
+++ chapter5/moretests.py
@@ -0,0 +1,28 @@
+# Some more conditions with lists
+
+first = "Hello"
+second = "hello"
+hello = "Hello"
+
+print(first == hello)
+print(second == hello)
+print(first.lower() == second.lower())
+
+# Let us play with numbers
+print("Numerical tests")
+print(2 > 3)
+print(2 < 3)
+print(2 <= 3)
+print(3 >= 2)
+
+# Clubbing conditions with and & or
+print(first.lower() == second and 2 < 3)
+print(first.lower() == second or 5 < 3)
+
+# List of numbers and some condition tests on list
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(10 in numbers)
+print(1 in numbers)
+print(10 not in numbers)
+print(1 not in numbers)
--- chapter5/tests.py
+++ chapter5/tests.py
@@ -7,6 +7,15 @@ exercise = 'running'
 print('Are you fit?')
 print(exercise == 'running')
 
+number = 2
+print("Is number even?")
+print(number % 2 == 0)
+
+capital = "Delhi"
+print("Is Delhi capital of India?")
+print(capital == 'Delhi')
+
+print("\n")
 food_had = 'dosa'
 print('Are you hungry?')
 print(food_had == '')
@@ -14,3 +23,11 @@ print(food_had == '')
 car = 'linea'
 print('Do you i own a BMW')
 print(car == 'bmw')
+
+number = 3
+print("Is number even?")
+print(number % 2 == 0)
+
+capital = "Mumbai"
+print("Is Mumbai capital of India?")
+print(capital == 'Delhi')
