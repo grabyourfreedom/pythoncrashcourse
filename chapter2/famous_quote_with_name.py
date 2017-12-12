d747f94c09b7382005d12f2ca2bffde4250e075a
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2017-12-11 22:26:28 -0800

    Added comments and followed PEP

--- chapter2/famous_quote_with_name.py
+++ chapter2/famous_quote_with_name.py
@@ -1,3 +1,6 @@
+# This is a little variation where we store the name of famous personality
+# in a variable and what they said in another variable.
+# Concatenate and print them
 name = "albert einstein"
 quote = 'A person who never made a mistake never tried anything new.”'
 
