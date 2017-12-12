c6e8c9bf5cd7a86ba93bf1237eeb158b48c25e22
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2017-12-11 22:29:42 -0800

    Added comments and followed PEP

--- chapter2/greeting.py
+++ chapter2/greeting.py
@@ -1,3 +1,5 @@
+# This program prints a personalized greeting and then asks whether
+# someone wants to learn Python (can you spot a PEP good practice?)
 first_name = "eric"
 message = "Hello, " + first_name.title() + \
           ", would you like to learn some Python today?"
