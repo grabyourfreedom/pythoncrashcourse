ee51cc4b30b381d54351b3aed0cdc7abddf72d07
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2017-12-11 22:15:20 -0800

    First commit

--- chapter1/hello_world.py
+++ chapter1/hello_world.py
@@ -1,3 +1,7 @@
+# This is our first program, printing "Hello, world" to
+# ensure that our environment is working fine
 print("Hello World!!!")
 
-print(Hello world)
+# The following line has an error. You should see an error
+# if you uncomment and run this line
+#print(Hello world)
