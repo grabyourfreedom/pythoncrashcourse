5c36e3a87acef6bcb29cdd96acea88e92dbc8c24
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2017-12-11 22:18:09 -0800

    Added comments and followed PEP

--- chapter2/famous_quote.py
+++ chapter2/famous_quote.py
@@ -1,1 +1,4 @@
+# This tiny program prints one of the famous quotes by Albert Einstein
+# (and if you have noticed, we have followed PEP guidance for number
+# of characters in a line)
 print('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
